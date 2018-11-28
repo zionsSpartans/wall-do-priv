8b7cdac2f7c7110f99d3dd30962a75bc9297f91e
Andoni A <andonialonsof@gmail.com>
2018-11-28 19:12:42 +0100

    Update repo tree

--- walldo_py/query-elk/query.py
+++
@@ -1,6 +0,0 @@
-from elasticsearch import Elasticsearch
- 
-es = Elasticsearch()
-#res = es.search(index="filebeat-6.4.2-2018.11.20", body={"query": {"match": {'hostname': 'localhost.localdomain'}}})
-es.get(index='filebeat-6.4.2-2018.11.20')
-
